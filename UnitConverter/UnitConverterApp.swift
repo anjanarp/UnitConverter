860c107fa8efce64ef20ea2d4db6e880774ec241
Anjana <anjanapcollege@gmail.com>
2024-08-30 21:04:49 +0530

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Anjana Rajamani on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
